67d79c515f471948347ed4cbf7ba24f85f1f9579
Jeremy Rubin <j@rubin.io>
2022-01-31 17:48:41 -0800

    Add Emoji Picker

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,51 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  async function save() {
+     await navigator.clipboard.writeText("HAHAH");
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Grid/>
+      <p> Copy and Paste to Save </p>
       </header>
     </div>
   );
+}
+
+function Grid() {
+
+    const row = () => (
+    <div>
+         <Cell/>
+         <Cell/>
+         <Cell/>
+         <Cell/>
+         <Cell/>
+     </div>
+    );
+    return (<div style={{display:"grid", gridTemplateRows: "1fr 1fr 1fr 1fr 1fr 1fr"}}>
+    {row()}
+    {row()}
+    {row()}
+    {row()}
+    {row()}
+    {row()}
+    </div>);
+}
+
+const colors = ["⬛️", "🟨", "🟩", "🟥", "🟦", "🟧", "🟪", "🟫", "⬜️"];
+function Cell() {
+    let [color, setColor] = React.useState(0); 
+    function click() {
+        setColor(color+1 % colors.length);
+    }
+    return (<span onClick={click}>
+        {colors[color]}
+    </span>);
 }
 
 export default App;
