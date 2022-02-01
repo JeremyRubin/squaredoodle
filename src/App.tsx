03f2012da552ce499b4e45dbbca50a2cfc9e1430
Jeremy Rubin <j@rubin.io>
2022-01-31 17:53:49 -0800

    Remove unuseds

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 
 function App() {
-  async function save() {
-     await navigator.clipboard.writeText("HAHAH");
-  }
   return (
     <div className="App">
       <header className="App-header">
